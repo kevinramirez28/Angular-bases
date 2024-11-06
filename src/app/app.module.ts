97fdcb2e90b24044e261c1250fff7ec19d97f7c4
kevinramirez28 <kevinramireztabres@gmail.com>
2024-11-06 22:47:21 +0100

    Working progress DbzModule not working in Angular18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 
 import { CounterModule } from './counter/components/counter/counter.module';
 import { HeroesModule } from './heroes/counter/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule // ahora como se exporto, pues ahora hay que importarlo para que se cree el enlace
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/counter/heroes.module.ts
+++ src/app/heroes/counter/heroes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../hero/hero.component';
 import { ListComponent } from '../list/list.component';
-import { CommonModule } from '@angular/common';
 
 
 @NgModule({
